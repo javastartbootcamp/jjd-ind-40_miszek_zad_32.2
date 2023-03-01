c2971eb034a89e84130d1d340ff96481657e6ee3
Michal Szekalski <szekalski.michal@gmail.com>
2023-03-01 22:49:46 +0100

    solution v1

--- src/main/java/pl/javastart/jpaoptimalization/MainController.java
+++ src/main/java/pl/javastart/jpaoptimalization/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.javastart.jpaoptimalization.country.Country;
 import pl.javastart.jpaoptimalization.country.CountryService;
+import pl.javastart.jpaoptimalization.country.CountryWithBiggestCity;
 import pl.javastart.jpaoptimalization.countrylanguage.CountryLanguage;
 import pl.javastart.jpaoptimalization.countrylanguage.CountryLanguageService;
 
@@ -24,28 +25,22 @@ public class MainController {
 
     @GetMapping("/najwieksze-miasta")
     public String countryWithBiggestCity(Model model) {
-        List<Country> countries = countryService.findAll();
+        List<CountryWithBiggestCity> countries = countryService.findAllWithBiggestCity();
         model.addAttribute("countries", countries);
-
         return "countryWithBiggestCity";
     }
 
     @GetMapping("/kraje-i-jezyki")
     public String countryWithLanguages(Model model) {
-        List<Country> countries = countryService.findAll();
-
+        List<Country> countries = countryService.findAllWithLanguages();
         model.addAttribute("countries", countries);
-
         return "countryWithLanguages";
     }
 
     @GetMapping("/jezyki-i-kraje")
     public String languagesWithCountries(Model model) {
         List<CountryLanguage> languages = countryLanguageService.findAll();
-
         model.addAttribute("languages", languages);
-
         return "languagesWithCountries";
     }
-
 }
